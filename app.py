db70be99766272286a87dc91ccb8c2395de323e0
NguyenNamVlog <nguyennam.tfvlog@gmail.com>
2026-01-02 20:36:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -250,12 +250,12 @@ def create_pdf(order, title):
     
     if is_delivery:
         odate = datetime.now().strftime("%d/%m/%Y")
-        intro_text = "Cong ty TNHH SX KD TM An Loc Phat xin cam on su quan tam cua Quy khach hang den san pham va dich vu cua chung toi. Nay ban giao cac hang hoa va dich vu nhu sau:"
+        intro_text = "Công ty TNHH SX KD TM An Lộc Phát xin cám ơn sự quan tâm của Quý khách hàng đến sản phẩm và dịch vụ của chúng tôi.  Nay bàn giao các hàng hóa và dịch vụ như sau:"
     else:
         raw_date = order.get('date', '')
         try: odate = datetime.strptime(raw_date, "%Y-%m-%d").strftime("%d/%m/%Y")
         except: odate = raw_date
-        intro_text = "Cong ty TNHH SX KD TM An Loc Phat xin cam on su quan tam cua Quy khach hang den san pham va dich vu cua chung toi. Xin tran trong gui toi Quy khach hang bao gia nhu sau:"
+        intro_text = "Công ty TNHH SX KD TM An Lộc Phát xin cám ơn sự quan tâm của Quý khách hàng đến sản phẩm và dịch vụ của chúng tôi. Xin trân trọng gửi tới Quý  khách hàng báo giá như sau:"
 
     cust = order.get('customer', {})
     items = order.get('items', [])
@@ -347,7 +347,7 @@ def create_pdf(order, title):
         pdf.multi_cell(190, 5, txt("* Giao hàng miễn phí trong nội thành thành phố Biên Hoà với đơn hàng >1.000.000đ"))
         pdf.ln(2)
         pdf.set_x(10)
-        pdf.multi_cell(190, 5, txt("Rất mong được hợp tác với Quý khách hàng. Trân trọng!"))
+        pdf.multi_cell(190, 5, txt("Rất mong được hợp tác với Quý khách hàng."))
     else:
         pdf.cell(0, 5, txt("Lưu ý:"), 0, 1)
         pdf.set_x(10)
